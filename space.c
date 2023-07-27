ad854ba772bd7893765985c544cda4be3838cf67
H11TM4N <jeremiahbrownie@gmail.com>
2023-07-27 06:57:29 +0100

    trial

--- space.c
+++ space.c
@@ -1,20 +1,27 @@
 #include "main.h"
 
+/**
+ * trim_whitespace - Removes leading and trailing whitespace from a string.
+ * @str: The input string to trim.
+ *
+ * Return: A pointer to the trimmed string.
+ */
+
 char *trim_whitespace(char *str)
 {
-    char *end;
+	char *end;
 
-    while (isspace((unsigned char)*str))
-        str++;
+	while (isspace((unsigned char)*str))
+		str++;
 
-    if (*str == 0)
-        return str;
+	if (*str == '\0')
+		return (str);
 
-    end = str + strlen(str) - 1;
-    while (end > str && isspace((unsigned char)*end))
-        end--;
+	end = str + strlen(str) - 1;
+	while (end > str && isspace((unsigned char)*end))
+		end--;
 
-    *(end + 1) = '\0';
+	*(end + 1) = '\0';
 
-    return str;
+	return (str);
 }
